9428a7559543c63c41445942874fde43cc33fed3
Yassin Arki <mac@yassins-Macbook-Pro.local>
2025-09-27 16:54:13 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/samalync.com/", //"/","
+  base: "/samalync.github.io", //"/","
   server: {
     host: "::",
     port: 8080,
