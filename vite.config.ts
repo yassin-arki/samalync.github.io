eed977936752aebd666de4e1147ed47229787a47
Yassin Arki <mac@yassins-Macbook-Pro.local>
2025-09-27 14:41:11 +0200

    set vite base to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/samalync.com/", //"/","
+  base: "/samalync.github.io/",//"/samalync.com/", //"/","
   server: {
     host: "::",
     port: 8080,
